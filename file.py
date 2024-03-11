6eaa568be75df81bde613f5cf750ffb582e32ea0
ADIKESAVA12 <162155897+ADIKESAVA12@users.noreply.github.com>
2024-03-11 15:57:28 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,37 @@
+print("hello world")
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+
+1)commenting is used for making documentation in a python file>
+2)when the line is connected,that line will not be considered as a part of the code.
+3)when the program is executed,commented line will not be in the part of execution.
+
+
+
+-----> NOTE:
+single line commanding can be done by using # at the begging og the line
+
+    Eg:
+        #print("hello world")
+
+
+Mutliple line commenting can be done ''' code '''. The lines between the quotes will be commented.
+"""
+"""
+Eg:
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+#print("hello world)
+
+
+import this
+# import is a keyword which is used to communicate between the python files.
